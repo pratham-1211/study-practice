984a2ea37d1f0264ff7055f2120c47cc81397ddd
pratham-1211 <120415594+pratham-1211@users.noreply.github.com>
2022-12-26 09:45:36 +0530

    Opening Chrome in incognito

--- SeleniumPractice/src/webelementhandling/EmailGetter.java
+++ SeleniumPractice/src/webelementhandling/EmailGetter.java
@@ -3,34 +3,46 @@ package webelementhandling;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class EmailGetter {
 
 	public static void main(String[] args) throws InterruptedException {
-WebDriver driver = new ChromeDriver();
-Actions action=new Actions(driver);
+		ChromeOptions option=new ChromeOptions();
+		option.addArguments("--incognito");
+		
+		DesiredCapabilities dc=new DesiredCapabilities();
+		dc.setCapability(ChromeOptions.CAPABILITY, option);
+		
+		WebDriver driver = new ChromeDriver(option);
+		
+		Actions action=new Actions(driver);
 
 driver.manage().deleteAllCookies();
 driver.manage().window().maximize();
 
 driver.get("https://www.convertcsv.com/email-extractor.htm");
-
-Thread.sleep(2000);
-
+Thread.sleep(3000);
 driver.findElement(By.id("fileTabLink")).click();
 
 action.moveToElement(driver.findElement(By.id("f1"))).perform();
-Thread.sleep(2000);
 action.moveToElement(driver.findElement(By.id("btnRun"))).perform();
-Thread.sleep(2000);
 System.out.println((driver.findElement(By.id("f1")).getAttribute("Class")));
-Thread.sleep(2000);
 
 
 driver.findElement(By.id("f1")).sendKeys("C:\\Users\\prath\\Downloads\\West Zone QA Jobs 22 Dec 2022.pdf");
 
-Thread.sleep(2000);
+//driver.get("https://www.ilovepdf.com/pdf_to_word");
+//action.moveToElement(driver.findElement(By.id("pickfiles"))).click().build().perform();
+//action.moveToElement(driver.findElement(By.id("pickfiles"))).perform();
+
+Thread.sleep(3000);
+driver.close();
+
+
+
 
 	}
 
